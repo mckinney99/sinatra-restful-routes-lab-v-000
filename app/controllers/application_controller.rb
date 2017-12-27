77d00eba1441f88abc9f92b61e564381e10259a9
Eric McKinney <mckinney99@gmail.com>
2017-12-27 17:10:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
+
   get '/' do
     erb :index
   end
 
   get '/recipes/new' do
     erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:title], :content => params[:content])
+    redirect to "/recipes/#{@recipe.id}"
   end
 end
